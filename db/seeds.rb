736b6a2b4521b295300161d86e14ade2069d8b16
haryu <hryau6@gmail.com>
2021-10-01 13:21:39 +0900

    add: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  restaurant = Restaurant.new(
+    name: "#{i}_restaurant",
+    fee: 100,
+    time_required: 10,
+  )
+
+  12.times do |m|
+    restaurant.foods.build(
+      name: "food_name_#{m}",
+      price: 500,
+      description: "description of food_name_#{m}"
+    )
+  end
+  restaurant.save!
+end
